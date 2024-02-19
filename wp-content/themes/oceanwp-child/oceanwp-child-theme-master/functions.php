f790751d595c9b22e67f8699f225cb0e3d80e01b
Nicolas Guapo <nicolas.lecoeur.guapo@gmail.com>
2024-02-19 15:41:40 +0100

    demarrage création hook

--- wp-content/themes/oceanwp-child/oceanwp-child-theme-master/functions.php
+++ wp-content/themes/oceanwp-child/oceanwp-child-theme-master/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * OceanWP Child Theme Functions
  *
@@ -19,15 +20,18 @@
  *
  * @link http://codex.wordpress.org/Child_Themes
  */
-function oceanwp_child_enqueue_parent_style() {
+function oceanwp_child_enqueue_parent_style()
+{
 
 	// Dynamically get version number of the parent stylesheet (lets browsers re-cache your stylesheet when you update the theme).
-	$theme   = wp_get_theme( 'OceanWP' );
-	$version = $theme->get( 'Version' );
+	$theme   = wp_get_theme('OceanWP');
+	$version = $theme->get('Version');
 
 	// Load the stylesheet.
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'oceanwp-style' ), $version );
-	
+	wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('oceanwp-style'), $version);
 }
 
-add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
+add_action('wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style');
+
+
+/*création du hook admin */
